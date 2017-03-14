11f86c842eae3ca8ee049749c0fa7d40f6b92a35
msedlazek <mark.sedlazek@gmail.com>
2017-03-14 17:15:55 -0400

    initial upload, taking code from activities.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Double Bacon Ranch Burger');
+INSERT INTO burgers (name) VALUES ('Maui Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Heart Attack Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Breakfast Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Notta Burger', true);
